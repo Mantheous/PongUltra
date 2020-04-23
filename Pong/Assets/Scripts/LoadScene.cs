422f2d345d7101479438d926716e08d80981f123
Mantheous <Mantheous@gmail.com>
2020-04-22 19:24:18 -0600

    Update Build

---
+++ Pong/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    public int sceneNumber;
+    public bool UseSpace;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && UseSpace)
+        {
+            Load();
+        }
+    }
+
+    public void Load()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
